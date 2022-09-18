b767b067e8a906eb8ec5b65902710e79631eaabe
mj07yadav <a9412446617526244@gmail.com>
2022-09-18 12:45:37 +0530

    spring boot project base

---
+++ src/main/java/com/mritunjay/mjrules/MjrulesApplication.java
@@ -0,0 +1,13 @@
+package com.mritunjay.mjrules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjrulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjrulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mritunjay/mjrules/MjrulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mritunjay.mjrules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjrulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
